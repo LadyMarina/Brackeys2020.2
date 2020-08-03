0ff57fa8d36d0db3c3554ea7de5f07b4da39bb74
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-08-03 16:09:37 +0200

    f

---
+++ RewindParty/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private float inputX;
+    private float inputY;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputX != 0 || inputY != 0)
+        {
+            Vector2 direction = Vector2.ClampMagnitude(new Vector2(inputX, inputY), 1f);
+
+            rb.MovePosition(new Vector2(rb.position.x + direction.x * movementSpeed * Time.deltaTime, rb.position.y + direction.y * movementSpeed * Time.deltaTime));
+        }
+
+    }
+}
