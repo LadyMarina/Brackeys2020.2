9916a6af871be252111d13bac9b1f722a1fba105
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-08-03 18:09:57 +0200

    ClockAndMoney

---
+++ RewindParty/Assets/Scripts/Clock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Threading;
+
+public class Clock : MonoBehaviour
+{
+    public bool pause;
+
+    [SerializeField] private int startHour, startMinutes, startSeconds;
+
+    /// <summary>
+    /// Speed at which time goes
+    /// </summary>
+    [SerializeField] private float timeSpeed = 1;
+
+    private float timer;
+    private Text textClock;
+
+    void Awake()
+    {
+        textClock = GetComponent<Text>();
+
+        timer += startHour * 3600;
+        timer += startMinutes * 60;
+        timer += startSeconds;
+    }
+
+    void Update()
+    {
+        if (!pause)
+        {
+            timer += Time.deltaTime * timeSpeed;
+
+            if(Mathf.Floor((timer % 216000) / 3600).ToString("00") == "24")
+            {
+                timer = 0;
+            }
+
+            string hours = Mathf.Floor((timer % 216000) / 3600).ToString("00");
+            string minutes = Mathf.Floor((timer % 3600) / 60).ToString("00");
+            string seconds = (timer % 60).ToString("00");
+
+            textClock.text = hours + ":" + minutes + ":" + seconds;
+
+            
+        }
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+
+}
---
+++ RewindParty/Assets/Scripts/Money.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    private long money;
+
+    [SerializeField] private GameObject loseMoneyGameObject;
+
+    private Text text;
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void AddMoney(int moneyToAdd)
+    {
+        money += moneyToAdd;
+
+        UpdateText();
+    }
+
+    public void LoseMoney(int moneyToLose)
+    {
+        moneyToLose -= moneyToLose;
+
+        UpdateText();
+
+        loseMoneyGameObject = Instantiate(loseMoneyGameObject, transform.parent);
+
+        loseMoneyGameObject.GetComponent<Text>().text = "-" + moneyToLose + "$";
+    }
+    
+    public long GetMoney()
+    {
+        return money;
+    }
+
+    private void UpdateText()
+    {
+        text.text = money.ToString() + "$";
+    }
+}
