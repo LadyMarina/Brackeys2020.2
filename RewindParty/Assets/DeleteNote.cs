86e34a730a18d9a5f770aa9a3506b91096961333
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-08-03 23:45:33 +0200

    Notes Section and some Ui and scripts elements for UI.

---
+++ RewindParty/Assets/DeleteNote.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteNote : MonoBehaviour
+{
+    public Transform parentCanvas;
+
+    private bool getDeleted = false;
+    private float speed = 500;
+
+    public void DestroyNote()
+    {
+        transform.SetParent(transform.parent.parent);
+        getDeleted = true;
+        Destroy(gameObject, 5f);
+    }
+
+    private void Update()
+    {
+        if (getDeleted)
+        {
+            transform.position += (Vector3)Vector2.right * Time.deltaTime * speed;
+        }
+    }
+}
