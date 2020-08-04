168418c644d95b1ee99417bd1bd46cd3ce97a909
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-08-04 13:06:46 +0200

    Notes and Game Manager

---
+++ RewindParty/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public List<string> notes;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        notes = new List<string>();
+    }
+
+    public void AddNote(string text)
+    {
+        notes.Add(text);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
